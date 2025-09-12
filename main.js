d9adc6db1aefedcbab8a7d1c4f735a6aaefec802
kien912 <kientrung4902@gmail.com>
2025-09-12 21:30:37 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Tạo một phần tử audio
-const audio = new Audio('ngu.mp3');
-
-// Tự động phát nhạc khi trang được tải
-window.addEventListener('load', () => {
-  audio.play().catch(error => {
-    console.log('Auto play bị chặn bởi trình duyệt:', error);
-  });
-});
